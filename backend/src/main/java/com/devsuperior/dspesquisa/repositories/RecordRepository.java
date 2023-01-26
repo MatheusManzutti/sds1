099707669fd44c2dea3bfa1ed4a7dcbde27de000
Matheus Manzutti <mateusmanzutti@gmail.com>
2023-01-26 02:20:08 -0300

    Fix error query RecordRepository

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -14,8 +14,8 @@ import com.devsuperior.dspesquisa.entities.Record;
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
 	@Query("SELECT obj FROM Record obj WHERE "
-			+ "(:min IS NULL OR obj.moment >= :min) AND "
-			+ "(:max IS NULL OR obj.moment <= :max)") //parou em 2:19:35
+			+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+			+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max)") //parou em 2:19:35
 	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 	
 }
